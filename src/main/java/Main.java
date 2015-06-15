8884fe558b822dce98e872ce33d2ee459cd51ca8
Jean Abed <jean.abed@hotmail.com>
2015-06-15 20:19:13 +1000

    update to pom

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,5 +3,6 @@ package main.java;
 public class Main {
     public static void main(String[] args) {
         DBDriver d = SQLUtils.getDriver("mysql");
+        System.out.print(d.toString());
     }
 }
